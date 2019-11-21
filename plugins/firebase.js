fd8082d5470705c610f09c65aab8fc4eb0892ab4
keinuma <nununu.mono@gmail.com>
2019-11-22 01:07:38 +0900

    Delete secret

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,6 @@ import 'firebase/firestore'
 if (!firebase.apps.length) {
   firebase.initializeApp({
     // TODO: Insert firebase setting
-    apiKey: "AIzaSyB22G2X41EHUnKA8WmslCOWMLybHyxsOn8",
-    authDomain: "sample-47036.firebaseapp.com",
-    databaseURL: "https://sample-47036.firebaseio.com",
-    projectId: "sample-47036",
-    storageBucket: "sample-47036.appspot.com",
-    messagingSenderId: "1039393971545",
-    appId: "1:1039393971545:web:1d5a2f89b490bf24254048"
   })
 }
 
